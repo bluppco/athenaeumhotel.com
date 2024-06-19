fe29c0c0054325697d1bd61791f9065e8c719009
akashgithubacc <rohansingh25december@gmail.com>
2024-06-19 22:01:29 +0530

    feat - Completed header and background image section

---
+++ src/content/headerContent/mainHeader.tsx
@@ -0,0 +1,14 @@
+//Tried to make this as an MDX file, but was not able to import
+//Content of First Row ie Main Header
+
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+export const navlinks: NavLink[] = [
+  { name: "Athenaeum Life", href: "/" },
+  { name: "Gift Vouchers", href: "/" },
+  { name: "Careers", href: "/" },
+  { name: "Manage Booking", href: "/" },
+];
---
+++ src/content/headerContent/subHeader.tsx
@@ -0,0 +1,19 @@
+//Tried to make this as an MDX file, but was not able to import
+//Content of Second Row ie Sub Header
+
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+export const navlinks: NavLink[] = [
+  { name: "Stay", href: "/" },
+  { name: "Residences", href: "/" },
+  { name: "Explore", href: "/" },
+  { name: "East & Drink", href: "/" },
+  { name: "Meet", href: "/" },
+  { name: "Spa", href: "/" },
+  { name: "Offers", href: "/" },
+  { name: "Group stays", href: "/" },
+  { name: "Esg", href: "/" },
+];
